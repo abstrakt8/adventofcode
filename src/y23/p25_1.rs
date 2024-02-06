3875fcd3e3063a89685e64bf0bc51557e1d5daf5
abstrakt <abstrakt.osu@gmail.com>
2024-02-06 00:15:53 +0000

    p25_1 faster, but still slow

--- src/y23/p25_1.rs
+++ src/y23/p25_1.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::cmp::min;
 use std::collections::HashMap;
 use std::iter::repeat;
 use std::slice::Iter;
@@ -8,6 +9,7 @@ use std::slice::Iter;
 #[derive(Default)]
 struct Dictionary<'a> {
     lookup: RefCell<HashMap<&'a str, usize>>,
+    reverse_lookup: RefCell<Vec<&'a str>>,
 }
 
 // A dictionary useful for translating ridiculous keys to usize which can be used for graphs
@@ -22,8 +24,12 @@ impl<'a> Dictionary<'a> {
 
             let mut lookup = self.lookup.borrow_mut();
             lookup.insert(name, new_id);
+            self.reverse_lookup.borrow_mut().push(name);
             new_id
         }
+    }
+    pub fn rev(&self, idx: usize) -> &str {
+        self.reverse_lookup.borrow()[idx]
     }
 }
 
@@ -66,47 +72,84 @@ impl FlexibleGraph {
 }
 
 struct DfsSpecial<'a> {
-    graph:  &'a FlexibleGraph,
-    vis: Vec<bool>,
-    ignore: [usize; 3],
+    dictionary: &'a Dictionary<'a>,
+    graph: &'a FlexibleGraph,
+    low: Vec<i32>,
+    disc: Vec<i32>,
+    time: usize,
+    ignore: [usize; 2],
+    ans: Option<usize>,
 }
 
 impl<'a> DfsSpecial<'a> {
-    pub fn new(graph: &'a FlexibleGraph) -> Self {
+    pub fn new(graph: &'a FlexibleGraph, dictionary: &'a Dictionary<'a>) -> Self {
+        let n = graph.number_of_vertices();
         Self {
-            graph: &graph,
-            vis: vec![false; graph.number_of_vertices()],
-            ignore: [0, 0, 0],
+            graph,
+            dictionary,
+
+            ignore: [0, 0],
+            ans: None,
+            disc: vec![-1; n],
+            low: vec![-1; n],
+            time: 0,
         }
     }
-    pub fn reset(&mut self, ignore: [usize; 3]) {
+    pub fn reset(&mut self, ignore: [usize; 2]) {
         self.ignore = ignore;
-        self.vis.fill(false);
+        self.ans = None;
+        self.disc.fill(-1);
+        self.low.fill(-1);
+        self.time = 0;
     }
 
-    fn dfs(&mut self, u: usize, p: usize) -> i32 {
-        let mut count = 1;
-        self.vis[u] = true;
+    fn edge(&self, i: usize) -> String {
+        let [u, v] = self.graph.edges[i];
+        format!("{} - {}", self.dictionary.rev(u), self.dictionary.rev(v))
+    }
+
+    fn dfs(&mut self, u: usize, p: usize) {
+        self.time += 1;
+        self.disc[u] = self.time as i32;
+        self.low[u] = self.time as i32;
+
         for (&v, ei) in self.graph.neighbors[u].iter().zip(self.graph.edge_indices[u].iter()) {
-            if v != p && !self.vis[v] && !self.ignore.contains(ei) {
-                count += self.dfs(v, u);
+            if v == p || self.ignore.contains(ei) { continue; }
+            if self.disc[v] == -1 {
+                let before_time = self.time;
+                self.dfs(v, u);
+                if self.low[v] > self.disc[u] {
+                    // It's assuming that there is only one bridge
+                    let subtree_size = self.time - before_time;
+                    let other_size = self.graph.number_of_vertices() - subtree_size;
+
+                    // println!("{subtree_size} x {other_size} time={} disc[{}]={} [{}, {}, {}]",
+                    //     self.time, u, self.disc[u],
+                    //      self.edge(self.ignore[0]), self.edge(self.ignore[1]), self.edge(*ei));
+                    self.ans = Some(subtree_size * other_size);
+                }
+                self.low[u] = min(self.low[u], self.low[v]);
+            } else { // back edge
+                self.low[u] = min(self.low[u], self.disc[v]);
             }
         }
-        count
     }
-    pub fn number_of_components(&mut self) -> Vec<i32> {
+    pub fn solve(&mut self) -> Option<usize> {
         let mut sizes = vec![];
         for u in 0..self.graph.number_of_vertices() {
-            if !self.vis[u] {
+            if self.disc[u] == -1 {
                 sizes.push(self.dfs(u, u));
             }
         }
-        sizes
+        if self.ans.is_some() {
+            println!("Sizes={}", sizes.len());
+        }
+        self.ans
     }
 }
 
 
-pub fn run(content: &str) -> i32 {
+pub fn run(content: &str) -> usize {
     let dict = Dictionary::default();
     let mut graph = FlexibleGraph::default();
     for line in content.lines() {
@@ -120,17 +163,15 @@ pub fn run(content: &str) -> i32 {
     // LOCK IT IN
     let graph = graph;
 
-    let mut dfs = DfsSpecial::new(&graph);
+    let mut dfs = DfsSpecial::new(&graph, &dict);
     println!("N={} E={}", graph.number_of_vertices(), graph.edges.len());
 
     for i in 0..graph.edges.len() {
+        println!("i={i}");
         for j in 0..i {
-            for k in 0..j {
-                dfs.reset([i, j, k]);
-                let sizes = dfs.number_of_components();
-                if sizes.len() == 2 {
-                    return sizes[0] * sizes[1];
-                }
+            dfs.reset([i, j]);
+            if let Some(ans) = dfs.solve() {
+                return ans
             }
         }
     }
@@ -157,6 +198,7 @@ lsr: lhk
 rzs: qnr cmg lsr rsh
 frs: qnr lhk lsr"##;
 
+        // https://dreampuf.github.io/GraphvizOnline/#graph%20G%20%7B%0A%20%20%20%20jqt%20--%20rhn%3B%0A%20%20%20%20jqt%20--%20xhk%3B%0A%20%20%20%20jqt%20--%20nvd%20%5Bcolor%3Dred%5D%3B%0A%20%20%20%20%0A%20%20%20%20rsh%20--%20frs%3B%0A%20%20%20%20rsh%20--%20pzl%3B%0A%20%20%20%20rsh%20--%20lsr%3B%0A%20%20%20%20%0A%20%20%20%20xhk%20--%20hfx%3B%0A%20%20%20%20%0A%20%20%20%20cmg%20--%20qnr%3B%0A%20%20%20%20cmg%20--%20nvd%3B%0A%20%20%20%20cmg%20--%20lhk%3B%0A%20%20%20%20cmg%20--%20bvb%20%5Bcolor%3Dblue%5D%3B%0A%20%20%20%20%0A%20%20%20%20rhn%20--%20xhk%3B%0A%20%20%20%20rhn%20--%20bvb%3B%0A%20%20%20%20rhn%20--%20hfx%3B%0A%20%20%20%20%0A%20%20%20%20bvb%20--%20xhk%3B%0A%20%20%20%20bvb%20--%20hfx%3B%0A%20%20%20%20%0A%20%20%20%20pzl%20--%20lsr%3B%0A%20%20%20%20pzl%20--%20hfx%20%5Bcolor%3Dgreen%5D%3B%0A%20%20%20%20pzl%20--%20nvd%3B%0A%20%20%20%20%0A%20%20%20%20qnr%20--%20nvd%3B%0A%20%20%20%20%0A%20%20%20%20ntq%20--%20jqt%3B%0A%20%20%20%20ntq%20--%20hfx%3B%0A%20%20%20%20ntq%20--%20bvb%3B%0A%20%20%20%20ntq%20--%20xhk%3B%0A%20%20%20%20%0A%20%20%20%20nvd%20--%20lhk%3B%0A%20%20%20%20%0A%20%20%20%20lsr%20--%20lhk%3B%0A%20%20%20%20%0A%20%20%20%20rzs%20--%20qnr%3B%0A%20%20%20%20rzs%20--%20cmg%3B%0A%20%20%20%20rzs%20--%20lsr%3B%0A%20%20%20%20rzs%20--%20rsh%3B%0A%20%20%20%20%0A%20%20%20%20frs%20--%20qnr%3B%0A%20%20%20%20frs%20--%20lhk%3B%0A%20%20%20%20frs%20--%20lsr%3B%0A%7D%0A
         assert_eq!(run(content), 54);
     }
 }
