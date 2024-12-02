cb0bd3f12051afc835f29bce7350b8f148c1f593
abstrakt <abstrakt.osu@gmail.com>
2024-12-02 15:53:37 +0000

    rayon

--- src/y24/p2.rs
+++ src/y24/p2.rs
@@ -1,26 +1,4 @@
-use std::cmp::{max, min};
-
-pub fn run_1(content: &str) -> u32 {
-    let res: u32 = content.lines().map(|line| {
-        let v: Vec<u32> = line.split_whitespace()
-            .map(|x| x.parse::<u32>().unwrap())
-            .collect();
-
-        let state = v.windows(2).fold(0, |state, sub| {
-            let diff = sub[1].abs_diff(sub[0]);
-            if diff == 0 || diff > 3 {
-                3
-            } else if sub[0] < sub[1] {
-                state | (1 << 0)
-            } else {
-                state | (1 << 1)
-            }
-        });
-        (state != 3) as u32
-    }).sum();
-    res
-}
-
+use rayon::prelude::*;
 
 // Bitmask represents whether it's still possible to keep the sequence increasing / decreasing if the bit is on
 const INCREASING: u8 = 1 << 0;
@@ -53,7 +31,7 @@ pub fn run(content: &str) -> u32 {
     // a = i - 2, 0
     // b = i - 1, 0
     // c = i - 1, 1
-    let res: u32 = content.lines().map(|line| {
+    let res: u32 = content.par_lines().map(|line| {
         let mut a: u8 = ALL;
         let mut b: u8 = ALL;
         let mut c: u8 = ALL;
