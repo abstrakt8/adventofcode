880bf41cfc938b0079cab02f18928b72b66de3b0
abstrakt <abstrakt.osu@gmail.com>
2024-12-02 11:54:19 +0000

    day2 2 fixed

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ use adventofcode::y24;
 pub fn main() -> Result<()> {
     color_eyre::install()?;
     let str = read_to_string("./inputs/y24/2.in")?;
-    // let str = read_to_string("./inputs/y24/2_example.in")?;
+    println!("ANS {}", y24::p2::run(&str));
+
+    let str = read_to_string("./inputs/y24/2_example.in")?;
     println!("ANS {}", y24::p2::run(&str));
 
     Ok(())
--- src/y24/p2.rs
+++ src/y24/p2.rs
@@ -1,25 +1,25 @@
 use std::cmp::min;
 
-// pub fn run_1(content: &str) -> u32 {
-//     let res: u32 = content.lines().map(|line| {
-//         let v: Vec<u32> = line.split_whitespace()
-//             .map(|x| x.parse::<u32>().unwrap())
-//             .collect();
-//
-//         let state = v.windows(2).fold(0, |state, sub| {
-//             let diff = sub[1].abs_diff(sub[0]);
-//             if diff == 0 || diff > 3 {
-//                 3
-//             } else if sub[0] < sub[1] {
-//                 state | (1 << 0)
-//             } else {
-//                 state | (1 << 1)
-//             }
-//         });
-//         (state != 3) as u32
-//     }).sum();
-//     res
-// }
+pub fn run_1(content: &str) -> u32 {
+    let res: u32 = content.lines().map(|line| {
+        let v: Vec<u32> = line.split_whitespace()
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+
+        let state = v.windows(2).fold(0, |state, sub| {
+            let diff = sub[1].abs_diff(sub[0]);
+            if diff == 0 || diff > 3 {
+                3
+            } else if sub[0] < sub[1] {
+                state | (1 << 0)
+            } else {
+                state | (1 << 1)
+            }
+        });
+        (state != 3) as u32
+    }).sum();
+    res
+}
 
 #[inline(always)]
 fn bit(a: u32, b: u32) -> u8 {
@@ -47,18 +47,19 @@ pub fn run(content: &str) -> u32 {
         let v: Vec<u32> = line.split_whitespace()
             .map(|x| x.parse::<u32>().unwrap())
             .collect();
-
         let n = v.len();
+
         // dp[i][k] = Whether subarray [0..i] is safe if we can skip k elements
         let mut dp: Vec<[u8; 2]> = vec![[0, 0]; n + 2];
 
         for i in 1..=n + 1 {
-            dp[i][0] = bitcoin(i - 1, i, &v) | dp[i - 1][0];
-            if i > 2 {
-                // Don't skip
-                dp[i][1] = min(dp[i - 1][1] | bitcoin(i - 1, i, &v),
-                               dp[i - 2][0] | bitcoin(i - 2, i, &v),
-                );
+            // Don't skip
+            dp[i][0] = dp[i - 1][0] | bitcoin(i - 1, i, &v);
+            dp[i][1] = dp[i - 1][1] | bitcoin(i - 1, i, &v);
+
+            // Skip
+            if i >= 2 {
+                dp[i][1] = min(dp[i][1], dp[i - 2][0] | bitcoin(i - 2, i, &v));
             }
         }
         // Skipping at least one is always better (you can skip the first element for example)
