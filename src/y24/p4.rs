afda24af11a6b7badf7b5c3ffd68cc4cc2e3f012
abstrakt <abstrakt.osu@gmail.com>
2024-12-04 20:07:25 +0000

    Rayon

--- src/y24/p4.rs
+++ src/y24/p4.rs
@@ -1,68 +1,66 @@
 // .XMASAMX
 // 01234567
+use rayon::prelude::*;
+use std::iter;
 #[inline(always)]
-pub fn automata(state: u8, next: u8) -> (u8, u32) {
-    match (state, next) {
-        (1, b'M') => (2, 0),
-        (2, b'A') => (3, 0),
-        (3, b'S') => (4, 1),
-        (4, b'A') => (5, 0),
-        (5, b'M') => (6, 0),
-        (6, b'X') => (1, 1),
-        _ =>
-            match next {
-                b'X' => (1, 0),
-                b'S' => (4, 0),
-                _ => (0, 0)
-            }
-    }
-}
-
-#[inline(always)]
-fn go(state: &mut u8, next: u8, cnt: &mut u32) {
-    let (next, new) = automata(*state, next);
-
-    *state = next;
-    *cnt += new;
-}
-
-pub fn run1(content: &str) -> u32 {
-    const N: usize = 150;
-    const D: usize = 2 * N + 1;
-    static mut cols: [u8; N] = [0; N];
-    static mut diag_se: [u8; D] = [0; D];
-    static mut diag_sw: [u8; D] = [0; D];
-    let mut row: u8 = 0;
-
-    let mut ans = 0;
-    let mut bi = 0;
-    let mut i = 0;
-    let mut j = 0;
-    let bytes = content.as_bytes();
-
-    // SAFETY: Because I say so
-    unsafe {
-        while bi < bytes.len() {
-            let b = *bytes.get_unchecked(bi);
-            bi += 1;
-
-            if b.is_ascii_whitespace() {
-                row = 0;
-                j = 0;
-                i += 1;
-                continue;
-            }
-
-            go(&mut row, b, &mut ans);
-            go(&mut cols[j], b, &mut ans);
-            go(&mut diag_se[i + j], b, &mut ans);
-            go(&mut diag_sw[N + i - j], b, &mut ans);
-
-            j += 1;
+pub fn go(state: &mut u8, next: u8, cnt: &mut u32) {
+    match (*state, next) {
+        (1, b'M') => *state = 2,
+        (2, b'A') => *state = 3,
+        (3, b'S') => {
+            *state = 4;
+            *cnt += 1;
+        }
+        (4, b'A') => *state = 5,
+        (5, b'M') => *state = 6,
+        (6, b'X') => {
+            *state = 1;
+            *cnt += 1;
+        }
+        _ => match next {
+            b'X' => *state = 1,
+            b'S' => *state = 4,
+            _ => *state = 0
         }
     }
-    ans
 }
+
+// Average: 637.573µs
+pub fn run1(content: &str) -> u32 {
+    let n = content.lines().count() as i32;
+    let m = content.lines().next().map_or(0, |line| line.len()) as i32;
+    // let m = n;
+
+    let rows = (0..n).map(|i| (i, 0i32, 0i32, 1i32));
+    let cols = (0..m).map(|j| (0, j, 1, 0));
+    let d_sw1 = (0..m).map(|j| (0, j, 1, -1));
+    let d_sw2 = (1..n).map(|i| (i, m - 1, 1, -1));
+    let d_se1 = (0..n).map(|i| (i, 0, 1, 1));
+    let d_se2 = (1..m).map(|j| (0, j, 1, 1));
+    let bytes = content.as_bytes();
+
+    let all = iter::empty()
+        .chain(rows)
+        .chain(cols)
+        .chain(d_sw1)
+        .chain(d_sw2)
+        .chain(d_se1)
+        .chain(d_se2);
+    all.par_bridge()
+        .map(|(mut i, mut j, di, dj)| {
+            let mut s: u8 = 0;
+            let mut c = 0;
+
+            while 0 <= i && i < n && 0 <= j && j < m {
+                go(&mut s, bytes[(i * (m + 1) + j) as usize], &mut c);
+                i += di;
+                j += dj;
+            }
+            c
+        })
+        .sum()
+}
+
 
 pub fn run2(content: &str) -> u32 {
     let g: Vec<Vec<char>> = content.lines().map(|line| line.chars().collect()).collect();
@@ -98,10 +96,13 @@ pub fn run2(content: &str) -> u32 {
         }
     }
 
+    // 1985
     ans
 }
 
 
 pub fn run(content: &str) -> u32 {
-    run2(content)
+    run1(content)
+    // 1: 2551
+    // 2: 1985
 }
