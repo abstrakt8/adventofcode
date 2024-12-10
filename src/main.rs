d26fa9c275def232babd6e0f9d4c426b30fbda01
abstrakt <abstrakt.osu@gmail.com>
2024-12-10 19:24:45 +0000

    d15

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use adventofcode::{y22, y24};
 use color_eyre::Result;
 use std::fs::read_to_string;
 
-const YEAR: u32 = 24;
-const DAY: u32 = 10;
+const YEAR: u32 = 22;
+const DAY: u32 = 15;
 
 pub fn main() -> Result<()> {
     color_eyre::install()?;
@@ -15,7 +15,7 @@ pub fn main() -> Result<()> {
         let file_name = format!("./inputs/y{YEAR}/{DAY}{suffix}");
         let input = read_to_string(&file_name);
         if let Ok(input) = input {
-            let ans = y24::d10::run(&input);
+            let ans = y22::d15::run(&input);
             println!("{file_name}: {:?}", ans);
         } else {
             println!("Skipped {file_name}");
--- src/y22/d15.rs
+++ src/y22/d15.rs
@@ -1,7 +1,9 @@
 use regex::Regex;
+use std::cmp::{max, min};
 use std::collections::HashSet;
+use std::ops::{Range, RangeInclusive};
 
-#[derive(Debug, Default, Ord, PartialOrd, Eq, PartialEq, Hash)]
+#[derive(Debug, Default, Ord, PartialOrd, Eq, PartialEq, Hash, Clone)]
 struct Point {
     x: i32,
     y: i32,
@@ -17,15 +19,12 @@ fn hamilton(p: &Point, q: &Point) -> u32 {
     p.x.abs_diff(q.x) + p.y.abs_diff(q.y)
 }
 
-pub fn run(content: &str) -> u32 {
-    let re = Regex::new(
-        r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)",
-    )
-    .unwrap();
+pub fn run(content: &str) -> u64 {
+    let re =
+        Regex::new(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)")
+            .unwrap();
 
-    let mut pts: HashSet<Point> = HashSet::new();
-
-    let beacons: Vec<(Point, Point)> = re
+    let signal_pairs: Vec<(Point, Point)> = re
         .captures_iter(content)
         .map(|capture| {
             let (_, [a, b, c, d]) = capture.extract();
@@ -37,24 +36,68 @@ pub fn run(content: &str) -> u32 {
             (Point::new(a, b), Point::new(c, d))
         })
         .collect();
+    let beacon_pos: HashSet<Point> = signal_pairs.iter().map(|(_, b)| b.clone()).collect();
 
-    let y_target : i32 = 10;
-    // let y_target = 2000000;
+    // let y_target : i32 = 10;
 
-    for (s, b) in beacons {
-        let mut dist = hamilton(&s, &b) as i32;
-        dist -= y_target.abs_diff(s.y) as i32;
+    let mut ans: Vec<Point> = vec![];
 
-        if dist < 0 {
-            continue;
-        }
-        for x in -dist..=dist {
-            let c = Point::new(s.x + x, y_target);
-            if c != b {
-                pts.insert(c);
+    let mut calc_intervals = |y_target: i32| {
+        let mut intervals: Vec<RangeInclusive<i32>> = vec![];
+        for (s, b) in &signal_pairs {
+            let mut dist = hamilton(&s, &b) as i32;
+            dist -= y_target.abs_diff(s.y) as i32;
+
+            if dist < 0 {
+                continue;
+            }
+
+            let x1 = s.x - dist;
+            let x2 = s.x + dist;
+
+            if b.y == y_target {
+                if x1 < b.x {
+                    intervals.push(x1..=b.x - 1);
+                }
+                if b.x < x2 {
+                    intervals.push(b.x + 1..=x2);
+                }
+            } else {
+                intervals.push(x1..=x2);
             }
         }
+        intervals.sort_by(|x, y| x.start().cmp(&y.start()));
+
+        let mut close: i32 = 0;
+        for range in intervals {
+            if close + 1 < *range.start() {
+                let potential = Point::new(close + 1, y_target);
+                if !beacon_pos.contains(&potential) {
+                    ans.push(potential);
+                    println!(
+                        "y={y_target} Range open between x={} and {}",
+                        close + 1,
+                        *range.start() - 1
+                    );
+                }
+            }
+            if close < *range.end() {
+                close = *range.end();
+            }
+        }
+    };
+
+    // let range = 0..=20;
+    let range = 0..=4000000;
+
+    for y in range {
+        calc_intervals(y);
     }
 
-    pts.len() as u32
+    if ans.len() == 1 {
+        let p = ans.first().unwrap();
+        (p.x as u64) * 4000000 + p.y as u64
+    } else {
+        0
+    }
 }
