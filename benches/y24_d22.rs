332710265683df9000f77e40335cef419baa4fa0
abstrakt <abstrakt.osu@gmail.com>
2024-12-22 17:15:41 +0000

    d22 bench setup

---
+++ benches/y24_d22.rs
@@ -0,0 +1,17 @@
+use adventofcode::y24;
+
+fn main() {
+    divan::main();
+}
+
+const INPUT: &str = include_str!("../inputs/y24/22.in");
+
+#[divan::bench(sample_count = 300)]
+fn p1() {
+    assert_eq!(17163502021, y24::d22::run1(INPUT));
+}
+
+#[divan::bench(sample_count = 10)]
+fn p2() {
+    assert_eq!(1938, y24::d22::run2(INPUT));
+}
--- src/y24/d22.rs
+++ src/y24/d22.rs
@@ -2,74 +2,99 @@ use std::collections::HashMap;
 use std::fmt::Debug;
 use std::iter::{once, repeat_with};
 
-fn mix(a: u64, b: u64) -> u64 {
+fn mix(a: u32, b: u32) -> u32 {
     a ^ b
 }
 
-fn prune(a: u64) -> u64 {
+fn prune(a: u32) -> u32 {
     a & (16777216 - 1) // 2^24 - 1
 }
 
-fn next_secret(a: u64) -> u64 {
+fn next_secret(a: u32) -> u32 {
     let a = prune(mix(a, a << 6));
     let a = prune(mix(a, a >> 5));
     let a = prune(mix(a, a << 11));
     a
 }
 
-pub fn run(content: &str) -> impl Debug {
-    let initial: Vec<u64> = content.lines().filter_map(|s| s.parse().ok()).collect();
+struct Solver {
+    data: Vec<Vec<u32>>,
+}
 
-    let all_secrets: Vec<Vec<_>> = initial
-        .into_iter()
-        .map(|mut s| {
-            once(s)
-                .chain(
-                    repeat_with(move || {
-                        s = next_secret(s);
-                        s
-                    })
-                    .take(2000),
-                )
-                .collect::<Vec<_>>()
-        })
-        .collect();
+impl Solver {
+    pub fn from_input(content: &str) -> Self {
+        let initial: Vec<u32> = content.lines().filter_map(|s| s.parse().ok()).collect();
 
-    let ans1: u64 = all_secrets.iter().map(|secrets| secrets[2000]).sum();
+        let data: Vec<Vec<_>> = initial
+            .into_iter()
+            .map(|mut s| {
+                once(s)
+                    .chain(
+                        repeat_with(move || {
+                            s = next_secret(s);
+                            s
+                        })
+                        .take(2000),
+                    )
+                    .collect::<Vec<_>>()
+            })
+            .collect();
 
-    let mut global: HashMap<Vec<i32>, u32> = HashMap::new();
-    for secrets in all_secrets {
-        let mut local: HashMap<Vec<i32>, u32> = HashMap::new();
-
-        let mut prev = 0;
-        let mut seq = Vec::new();
-
-        for (i, s) in secrets.iter().enumerate() {
-            let cur = (*s % 10) as u32;
-            if i > 0 {
-                let d = (cur as i32) - (prev as i32);
-                seq.push(d);
-                if seq.len() > 4 {
-                    seq.remove(0);
-                }
-                if seq.len() == 4 {
-                    local
-                        .entry(seq.to_owned())
-                        .or_insert(cur);
-                }
-            }
-            prev = cur;
-        }
-
-        for (def, max_val) in local {
-            global
-                .entry(def)
-                .and_modify(|v| *v += max_val)
-                .or_insert(max_val);
-        }
+        Self { data }
     }
 
-    let ans2 = global.iter().map(|(a, b)| (*b, a.clone())).max().unwrap();
+    pub fn part1(&self) -> u64 {
+        self.data.iter().map(|secrets| secrets[2000] as u64).sum()
+    }
 
+    pub fn part2(&self) -> u32 {
+        let mut global: HashMap<Vec<i32>, u32> = HashMap::new();
+        for secrets in self.data.iter() {
+            let mut local: HashMap<Vec<i32>, u32> = HashMap::new();
+
+            let mut prev = 0;
+            let mut seq = Vec::new();
+
+            for (i, s) in secrets.iter().enumerate() {
+                let cur = (*s % 10);
+                if i > 0 {
+                    let d = (cur as i32) - (prev as i32);
+                    seq.push(d);
+                    if seq.len() > 4 {
+                        seq.remove(0);
+                    }
+                    if seq.len() == 4 {
+                        local.entry(seq.to_owned()).or_insert(cur);
+                    }
+                }
+                prev = cur;
+            }
+
+            for (def, max_val) in local {
+                global
+                    .entry(def)
+                    .and_modify(|v| *v += max_val)
+                    .or_insert(max_val);
+            }
+        }
+
+        *global.values().max().unwrap()
+    }
+}
+
+pub fn run1(content: &str) -> u64 {
+    let solver = Solver::from_input(content);
+    solver.part1()
+}
+
+pub fn run2(content: &str) -> u32 {
+    let solver = Solver::from_input(content);
+    solver.part2()
+}
+
+pub fn run(content: &str) -> impl Debug {
+    let solver = Solver::from_input(content);
+    let ans1 = solver.part1();
+    let ans2 = solver.part2();
     (ans1, ans2)
 }
